055e623cff0ac3bac84c576d58e1d5e8f9edcac4
Kennedy Pham <kennedypham@Kennedys-MacBook-Air.local>
2026-04-12 22:58:35 -0700

    fix port binding for render

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def notify_user(username):
     return jsonify({"status": "ok"}), 200
 
 if __name__ == "__main__":
-    app.run(debug = os.environ.get("FLASK_DEBUG", "False") == "True")
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)),
+            debug=os.environ.get("FLASK_DEBUG", "False") == "True")
